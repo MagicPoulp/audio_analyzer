dcb1ea4e8ac74c0f1930e48e379ac33ad5d3b5b6
Thierry Vilmart <3144141+MagicPoulp@users.noreply.github.com>
2020-08-02 21:32:49 +0200

    ad the calculation of amplitudes and phases

--- lib/AudioAnalyzer.dart
+++ lib/AudioAnalyzer.dart
@@ -10,12 +10,16 @@ import 'package:path_provider/path_provider.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:fft/fft.dart';
 // another more powerful library could be used: smart_signal_processing
+import 'package:smart_signal_processing/smart_signal_processing.dart' as ssp;
+import 'package:my_complex/my_complex.dart';
 
 class AudioAnalyzer {
 
     var _recorder;
     var _file;
     var _assetsAudioPlayer;
+    var _amplitudes;
+    var _phases;
 
     makeFile() async {
         final directory = await getApplicationDocumentsDirectory();
@@ -31,7 +35,8 @@ class AudioAnalyzer {
         bool hasPermission = await FlutterAudioRecorder.hasPermissions;
 
         if (!hasPermission) {
-            throw new Exception('Invalid permissions for the FlutterAudioRecorder');
+            throw new Exception(
+                'Invalid permissions for the FlutterAudioRecorder');
         }
 
         var file = File(_file);
@@ -44,7 +49,8 @@ class AudioAnalyzer {
         // the octave 9 is not reachable with 16K as sample rate
         // And the human ear can hear up to 20KHz
         // https://www.sciencedirect.com/topics/engineering/nyquist-theorem
-        _recorder = FlutterAudioRecorder(_file, audioFormat: AudioFormat.WAV, sampleRate: 16000);
+        _recorder = FlutterAudioRecorder(
+            _file, audioFormat: AudioFormat.WAV, sampleRate: 16000);
         await _recorder.initialized;
 
         await _recorder.start();
@@ -77,6 +83,8 @@ class AudioAnalyzer {
             await makeFile();
         }
         var fft = await computeFFT();
+        makeFrequencyArrays(fft);
+        var temp = 1;
     }
 
     // In the README, we have an adb command to download the wav
@@ -96,7 +104,7 @@ class AudioAnalyzer {
         int lengthBytes = bytes.length - 44;
         // the Hann function wants a sample with size equal to a power of 2
         // https://stackoverflow.com/questions/466204/rounding-up-to-next-power-of-2/24844826
-        var powerOf2 = pow(2, (log(lengthBytes)/log(2)).ceil())/2;
+        var powerOf2 = pow(2, (log(lengthBytes) / log(2)).ceil()) / 2;
         int extra = (lengthBytes - powerOf2) ~/ 2;
 
         ByteBuffer buffer = bytes.buffer;
@@ -107,6 +115,9 @@ class AudioAnalyzer {
 
         // a window function is necessary because the sample is not-periodic
         // but we can make it periodic
+        // A quote about the Hann function:
+        // "An example of apodization is the use of the Hann window in the fast Fourier transform analyzer to smooth the discontinuities at the beginning and end of the sampled time record. "
+        // https://en.wikipedia.org/wiki/Apodization
         var windowPackage = new Window(WindowType.HANN);
         var window = windowPackage.apply(samples);
         var fft = new FFT().Transform(window);
@@ -116,4 +127,17 @@ class AudioAnalyzer {
     // "In an fft frequency plot, the highest frequency is the sampling frequency fs and the lowest frequency is fs/N where N is the number of fft points. "
     // https://www.researchgate.net/post/How_can_I_define_the_frequency_resolution_in_FFT_And_what_is_the_difference_on_interpreting_the_results_between_high_and_low_frequency_resolution
     // The Nyquist-Shannon theorem says that we have an accuracy up to sample rate / 2.
+
+    makeFrequencyArrays(fft) async {
+        _amplitudes = new List(fft.length);
+        _phases = new List(fft.length);
+        for( var i = 0 ; i  < fft.length; i++) {
+            Complex v = fft[i];
+            var modulus = v.modulus;
+            // https://en.wikipedia.org/wiki/Complex_number
+            var phase = atan2(v.imaginary, v.real);
+            _amplitudes[i] = modulus;
+            _phases[i] = phase;
+        }
+    }
 }
