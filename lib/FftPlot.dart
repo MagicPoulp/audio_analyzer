09793331525d503c697b085ad92ecd07a94350eb
Thierry Vilmart <3144141+MagicPoulp@users.noreply.github.com>
2020-08-03 07:23:51 +0200

    cleanup

--- lib/FftPlot.dart
+++ lib/FftPlot.dart
@@ -16,7 +16,7 @@
 /// Scatter plot chart example
 // EXCLUDE_FROM_GALLERY_DOCS_START
 import 'dart:math';
-// EXCLUDE_FROM_GALLERY_DOCS_END
+
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
@@ -46,125 +46,9 @@ class SimpleScatterPlotChart extends StatelessWidget {
     );
   }
 
-  /// Creates a [ScatterPlotChart] with sample data and no transition.
-  factory SimpleScatterPlotChart.withSampleData() {
-    return new SimpleScatterPlotChart(
-      _createSampleData(),
-      // Disable animations for image tests.
-      animate: false,
-    );
-  }
-
-  // EXCLUDE_FROM_GALLERY_DOCS_START
-  // This section is excluded from being copied to the gallery.
-  // It is used for creating random series data to demonstrate animation in
-  // the example app only.
-  factory SimpleScatterPlotChart.withRandomData() {
-    return new SimpleScatterPlotChart(_createRandomData());
-  }
-
-  /// Create random data.
-  static List<charts.Series<LinearSales, num>> _createRandomData() {
-    final random = new Random();
-
-    final makeRadius = (int value) => (random.nextInt(value) + 2).toDouble();
-
-    final data = [
-      new LinearSales(random.nextInt(100), random.nextInt(100), makeRadius(6)),
-      new LinearSales(random.nextInt(100), random.nextInt(100), makeRadius(6)),
-      new LinearSales(random.nextInt(100), random.nextInt(100), makeRadius(6)),
-      new LinearSales(random.nextInt(100), random.nextInt(100), makeRadius(6)),
-      new LinearSales(random.nextInt(100), random.nextInt(100), makeRadius(6)),
-      new LinearSales(random.nextInt(100), random.nextInt(100), makeRadius(6)),
-      new LinearSales(random.nextInt(100), random.nextInt(100), makeRadius(6)),
-      new LinearSales(random.nextInt(100), random.nextInt(100), makeRadius(6)),
-      new LinearSales(random.nextInt(100), random.nextInt(100), makeRadius(6)),
-      new LinearSales(random.nextInt(100), random.nextInt(100), makeRadius(6)),
-      new LinearSales(random.nextInt(100), random.nextInt(100), makeRadius(6)),
-      new LinearSales(random.nextInt(100), random.nextInt(100), makeRadius(6)),
-    ];
-
-    final maxMeasure = 100;
-
-    return [
-      new charts.Series<LinearSales, int>(
-        id: 'Sales',
-        colorFn: (LinearSales sales, _) {
-          // Color bucket the measure column value into 3 distinct colors.
-          final bucket = sales.sales / maxMeasure;
-
-          if (bucket < 1 / 3) {
-            return charts.MaterialPalette.blue.shadeDefault;
-          } else if (bucket < 2 / 3) {
-            return charts.MaterialPalette.red.shadeDefault;
-          } else {
-            return charts.MaterialPalette.green.shadeDefault;
-          }
-        },
-        domainFn: (LinearSales sales, _) => sales.year,
-        measureFn: (LinearSales sales, _) => sales.sales,
-        data: data,
-      )
-    ];
-  }
-  // EXCLUDE_FROM_GALLERY_DOCS_END
-
   @override
   Widget build(BuildContext context) {
     // a LineChart is faster
     return new charts.NumericComboChart(seriesList, animate: animate);
   }
-
-  /// Create one series with sample hard coded data.
-  static List<charts.Series<LinearSales, int>> _createSampleData() {
-    final data = [
-      new LinearSales(0, 5, 3.0),
-      new LinearSales(10, 25, 5.0),
-      new LinearSales(12, 75, 4.0),
-      new LinearSales(13, 225, 5.0),
-      new LinearSales(16, 50, 4.0),
-      new LinearSales(24, 75, 3.0),
-      new LinearSales(25, 100, 3.0),
-      new LinearSales(34, 150, 5.0),
-      new LinearSales(37, 10, 4.5),
-      new LinearSales(45, 300, 8.0),
-      new LinearSales(52, 15, 4.0),
-      new LinearSales(56, 200, 7.0),
-    ];
-
-    final maxMeasure = 300;
-
-    return [
-      new charts.Series<LinearSales, int>(
-        id: 'Sales',
-        // Providing a color function is optional.
-        colorFn: (LinearSales sales, _) {
-          // Bucket the measure column value into 3 distinct colors.
-          final bucket = sales.sales / maxMeasure;
-
-          if (bucket < 1 / 3) {
-            return charts.MaterialPalette.blue.shadeDefault;
-          } else if (bucket < 2 / 3) {
-            return charts.MaterialPalette.red.shadeDefault;
-          } else {
-            return charts.MaterialPalette.green.shadeDefault;
-          }
-        },
-        domainFn: (LinearSales sales, _) => sales.year,
-        measureFn: (LinearSales sales, _) => sales.sales,
-        // Providing a radius function is optional.
-        radiusPxFn: (LinearSales sales, _) => sales.radius,
-        data: data,
-      )
-    ];
-  }
-}
-
-/// Sample linear data type.
-class LinearSales {
-  final int year;
-  final int sales;
-  final double radius;
-
-  LinearSales(this.year, this.sales, this.radius);
 }
