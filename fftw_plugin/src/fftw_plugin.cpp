c07e231e66d41ba18fe05e53f04a69b15ac04969
Thierry Vilmart <3144141+MagicPoulp@users.noreply.github.com>
2020-08-05 08:00:43 +0200

    first files for the plugin

---
+++ fftw_plugin/src/fftw_plugin.cpp
@@ -0,0 +1,18 @@
+
+#include <fftw.h>
+
+#include "fftw_plugin.h"
+
+// https://flutter.dev/docs/development/platform-integration/c-interop
+// http://www.fftw.org/fftw2_doc/fftw_2.html
+int fft() {
+     fftw_complex in[N], out[N];
+     fftw_plan p;
+
+     p = fftw_create_plan(N, FFTW_FORWARD, FFTW_ESTIMATE);
+
+     fftw_one(p, in, out);
+
+     fftw_destroy_plan(p);
+     return 0;
+}
---
+++ fftw_plugin/src/fftw_plugin.h
@@ -0,0 +1,16 @@
+//
+// Created by thierry on 2020-08-05.
+//
+
+#ifndef AUDIO_ANALYZER_FFTW_PLUGIN_H
+#define AUDIO_ANALYZER_FFTW_PLUGIN_H
+
+
+
+class fftw_plugin {
+    int fft();
+};
+
+
+
+#endif //AUDIO_ANALYZER_FFTW_PLUGIN_H
