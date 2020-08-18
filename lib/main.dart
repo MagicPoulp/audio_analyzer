e8b4f45592613cbe48b7da9ec1c2d4bd94575fa3
Thierry Vilmart <3144141+MagicPoulp@users.noreply.github.com>
2020-08-18 07:22:03 +0200

    improve the top bar

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class AudioAnalyzerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Audio analyzer',
       theme: ThemeData(
         // This is the theme of your application.
@@ -22,7 +23,7 @@ class AudioAnalyzerApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
